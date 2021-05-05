4af1d39e37fa920dbddaf0eb221923f58a5ed1c8
Lisa Pessin <lisapessin@gmail.com>
2021-05-05 09:08:15 -0400

    updated schema

--- schema.sql
+++ schema.sql
@@ -8,34 +8,34 @@ USE employeetracker_DB;
 -- Departments Table --
 -- Contains Department ID and Department Name --
 CREATE TABLE departments(
-    id INT NOT NULL AUTO_INCREMENT,
+    department_id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (department_id)
 );
 
 
 -- Roles Table --
 -- Contains Role ID, Title, Salary, and Department --
 CREATE TABLE roles(
-    id INT NOT NULL AUTO_INCREMENT,
+    role_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
 
 -- Employees Table --
 -- Contains Employee ID, First Name, Last Name, Role, and Manager --
 CREATE TABLE employees(
-    id INT NOT NULL AUTO_INCREMENT,
+    employee_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES roles(id)
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
 );
 
