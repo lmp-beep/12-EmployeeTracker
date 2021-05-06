e8dc762e3de01db60ebc0529fe509608e99b3537
Lisa Pessin <lisapessin@gmail.com>
2021-05-06 13:44:10 -0400

    working on add functions

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 const connection = require('./connection');
 
+const deptIds = [];
+
+
 
 
 message();
@@ -65,14 +68,14 @@ function mainMenu() {
             case 'View All Roles':
                 viewAllRoles();
                 break;
-            case 'Add New Employee':
-                addNewEmployee();
-                break;
             case 'Add New Department':
                 addNewDepartment();
                 break;
             case 'Add New Role':
                 addNewRole();
+                break;
+            case 'Add New Employee':
+                addNewEmployee();
                 break;
             case 'Update Employee Role':
                 updateEmployeeRole();
@@ -88,9 +91,10 @@ function mainMenu() {
 // *****NEEDS MANAGER NAME*********************************************
 const viewAllEmployees = () => {
     // show employee_id, first_name, last_name, title, salary, department, manager
-    const allEmployeeQuery = `SELECT employees.employee_id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.department_name FROM employees
+    const allEmployeeQuery = `SELECT employees.employee_id, employees.first_name, employees.last_name, employees.manager_id, roles.title, roles.salary, departments.department_name FROM employees
     INNER JOIN roles on roles.role_id = employees.role_id
     INNER JOIN departments on departments.department_id = roles.department_id`;
+    // INNER JOIN employees on employees.manager_id = employees.last_name`;
 
     connection.query(allEmployeeQuery, (err, results) => {
         if (err) throw err;
@@ -103,6 +107,7 @@ const viewAllEmployees = () => {
 const viewAllDepartments = () => {
     // show department_id, department_name (and total salaries BONUS)
     const allDepartmentsQuery = `SELECT departments.department_id, departments.department_name FROM departments`;
+
     connection.query(allDepartmentsQuery, (err, results) => {
         if (err) throw err;
         console.log(chalk.green('***ALL DEPARTMENTS***'));
@@ -114,8 +119,8 @@ const viewAllDepartments = () => {
 const viewAllRoles = () => {
     // show role_id, title, department, salary
     const allRolesQuery = `SELECT roles.role_id, roles.title, roles.salary, departments.department_name FROM roles
-    
     INNER JOIN departments on departments.department_id = roles.department_id`;
+
     connection.query(allRolesQuery, (err, results) => {
         if (err) throw err;
         console.log(chalk.green('***ALL ROLES***'));
@@ -124,19 +129,80 @@ const viewAllRoles = () => {
     })
 };
 
+const addNewDepartment = () => {
+    inquirer.prompt([
+        {
+            name: 'newDepartmentName',
+            type: 'input',
+            message: 'What is the name of the new department?',
+        },
+    ])
+    .then((answer) => {
+        connection.query(
+            'INSERT INTO departments SET ?',
+            {
+                department_name: answer.newDepartmentName,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log("Your new department was created successfully!");
+                viewAllDepartments();
+            }
+        );
+    });
+};
+   
+// *****JOIN DEPT ID and DEPT NAME*******************************************
+const addNewRole = () => {
+    connection.query('SELECT * FROM departments', function (err, res) {
+        if (err) throw err;
+        const roles = res.map(element => {
+            return element.department_name
+        })
+        inquirer.prompt([
+            {
+                name: 'newRoleTitle',
+                type: 'input',
+                message: "What is the name of the new role?",
+            },
+            {
+                name: 'newRoleDepartment',
+                type: 'list',
+                message: "What department is the new role in?",
+                choices: roles,
+            },
+            {
+                name: 'newRoleSalary',
+                type: 'input',
+                message: "What is the salary of the new role?",
+            },
+        ])
+        .then((answer) => {
+            connection.query(
+                'INSERT INTO roles SET ?',
+                {
+                    title: answer.newRoleTitle,
+                    department_id: answer.newRoleDepartment,
+                    salary: answer.newRoleSalary,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log("Your new role was created successfully!");
+                    viewAllRoles();
+                }
+            );
+        });
+    })
+};
+
+
+
+
 // addNewEmployee()
     // first name?
     // last name?
     // role? from list
     // manager? from list
-
-// addNewDepartment()
-    // department name?
-
-// addNewRole()
-    // role name?
-    // department?
-    // salary?
 
 // updateEmployeeRole()
     // which employee do you want to update? from list
